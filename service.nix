c00ce4a908a606790b569ce24f36361b371cc40a
regular <jan@lagomorph.de>
2025-03-07 10:13:15 +0100

    Correct DEBUG=

--- service.nix
+++ service.nix
@@ -156,7 +156,7 @@ in {
             RuntimeDirectory = runtimePath name;
             StateDirectory = "tre-server/${name}";
             Environment = [
-              "DEBUG=multiserver*,tre-cli-server:*"
+              "DEBUG=multiserver*,tre-server:*"
             ];
           };
         };
